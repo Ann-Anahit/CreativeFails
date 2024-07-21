21db8e97bdf3415c27438ded01b32ec97a81b9de
Anna Afijan <ann.afijan@gmail.com>
2024-07-21 10:29:16 +0000

    finish django setup

--- application/application/settings.py
+++ application/application/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_vite",
 ]
 
 MIDDLEWARE = [
@@ -117,9 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = "/staticfiles"
+STATIC_URL = "/static/"
+DJANGO_VITE_ASSETS_PATH = BASE_DIR / "static" / "map" / "dist"
+STATICFILES_DIRS = [str(BASE_DIR / "static"), DJANGO_VITE_ASSETS_PATH]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django vite mode
+
+DJANGO_VITE_DEV_MODE = True
