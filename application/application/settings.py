4231ada7c2be8527c10ec801b890b61c0e3d9f73
Anna Afijan <ann.afijan@gmail.com>
2024-07-26 16:46:34 +0000

    change concept

--- application/application/settings.py
+++ application/application/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "application"
 ]
 
 MIDDLEWARE = [
@@ -76,12 +77,15 @@ WSGI_APPLICATION = 'application.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sport_pond_doing_88171',
+        'USER': 'u88zg4h2jvc',
+        'PASSWORD': 'XTugDYbhDMYL',
+        'HOST': 'ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech',
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
