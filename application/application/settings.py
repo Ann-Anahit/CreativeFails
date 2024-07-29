a7cce170065c20cffa05777264a1bcd0125a3bb5
Anna Afijan <ann.afijan@gmail.com>
2024-07-29 18:16:37 +0000

    add accounts app and create register.html

--- application/application/settings.py
+++ application/application/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "application",
+    "accounts",
+    "django.contrib.messages",
 ]
 
 MIDDLEWARE = [
