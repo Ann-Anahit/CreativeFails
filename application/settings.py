55ea77fa7e111cd811ddf71ce43cc84a026ae408
Anna Afijan <ann.afijan@gmail.com>
2024-08-03 15:27:04 +0000

    add two models to django

--- application/settings.py
+++ application/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "map",
     "creative_fails", 
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -142,8 +143,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = "accounts.CustomUser"
 
 
 
