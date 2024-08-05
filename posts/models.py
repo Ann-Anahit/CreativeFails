724a14dfa0bcdef9b0f0ae083e2333db7ce7251a
Anna Afijan <ann.afijan@gmail.com>
2024-08-05 15:18:01 +0000

    make changes in model

--- posts/models.py
+++ posts/models.py
@@ -5,9 +5,11 @@ from accounts.models import CustomUser
 
 class Post(models.Model):  
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)  
-    title = models.CharField(max_length=200)  
+    title = models.CharField(max_length=200) 
+
     content = models.TextField()  
     created_at = models.DateTimeField(default=timezone.now)  
+
     visibility = models.BooleanField(default=True)  
 
     def __str__(self):  
