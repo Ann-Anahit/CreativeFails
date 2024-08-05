8b92d35a6814980b94e4807bf6d40b121bc789de
Anna Afijan <ann.afijan@gmail.com>
2024-08-05 11:01:44 +0000

    chenge views

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,7 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('index')
+            return redirect('write_article')
     else:
         form = CustomUserCreationForm()
     
