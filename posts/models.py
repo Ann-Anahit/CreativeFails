6591174f98c2977542eca0dd2ff19348c107056a
Anna Afijan <ann.afijan@gmail.com>
2024-08-07 18:11:20 +0000

    update a lot

--- posts/models.py
+++ posts/models.py
@@ -17,6 +17,6 @@ class Comment(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)  
     content = models.TextField()  
     created_at = models.DateTimeField(default=timezone.now) 
-     
+    
     def __str__(self):  
         return f"Comment by {self.user.username} on post {self.post.id}"
