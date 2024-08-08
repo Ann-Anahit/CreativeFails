6c2dd6a20970e4024649fd5fd5d3de7eff53e4c7
Anna Afijan <ann.afijan@gmail.com>
2024-08-08 10:43:36 +0000

    change settings

--- application/settings.py
+++ application/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'application.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'accounts/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
