419deec92409678fbd79511ee4a353c9748c82c4
Anna Afijan <ann.afijan@gmail.com>
2024-08-09 23:53:06 +0000

    set debug to false

--- application/settings.py
+++ application/settings.py
@@ -36,7 +36,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '8000-annanahit-creativefails-eunzjv2fi27.ws.codeinstitute-ide.net', '.herokuapp.com']
 
