60cce1bffebbe15d9e8059ffd05cac69afc10b87
Anna Afijan <ann.afijan@gmail.com>
2024-10-12 20:17:44 +0000

    change login

--- accounts/models.py
+++ accounts/models.py
@@ -34,8 +34,8 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
 
     def __str__(self):
         return self.email
--- creativefails/settings.py
+++ creativefails/settings.py
@@ -38,14 +38,14 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    '8000-annanahit-creativefails-w1lxi9mga0d.ws.codeinstitute-ide.net',
+    '8000-annanahit-creativefails-8udd0a6m0nc.ws.codeinstitute-ide.net',
     '.herokuapp.com'
 ]
 CSRF_TRUSTED_ORIGINS = [
