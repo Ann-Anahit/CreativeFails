badbc7de4a1c1076462063a949d13fb179cc4601
Anna Afijan <ann.afijan@gmail.com>
2024-10-13 21:31:37 +0000

    change post models

--- posts/models.py
+++ posts/models.py
@@ -7,16 +7,14 @@ from django.contrib.auth.models import User
 from django.conf import settings
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
     content = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # Use AUTH_USER_MODEL here
     created_at = models.DateTimeField(auto_now_add=True)
-    published = models.BooleanField(default=True)  # This should be present in your model
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
-    featured_image = models.ImageField(upload_to='featured_images/', blank=True, null=True)
-    
+
     def __str__(self):
-        return self.title 
+        return self.title
 
 class Comment(models.Model):  
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")  
