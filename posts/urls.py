4374cf8d1b171151c6deaa942696daa3a67571a7
Anna Afijan <ann.afijan@gmail.com>
2024-10-15 00:43:12 +0000

    change urls path

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-from .views import post_like, unlike_post
+from .views import post_like, post_unlike
 
 
 
@@ -16,5 +16,5 @@ urlpatterns = [
     path('comments/edit/<int:comment_id>/', views.edit_comment, name='edit_comment'),
     path('comments/delete/<int:comment_id>/', views.delete_comment, name='delete_comment'),
     path('like<int:post_id>/', post_like, name='post_like'),
-    path('unlike/<int:post_id>/', unlike_post, name='post_unlike'),
+    path('unlike/<int:post_id>/', post_unlike, name='post_unlike'),
 ]
