817e30e63a30870946ac5d57b7fdbc68d694975c
Anna Afijan <ann.afijan@gmail.com>
2024-12-20 22:38:54 +0000

    remove the success message

--- accounts/views.py
+++ accounts/views.py
@@ -18,7 +18,6 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            messages.success(request, 'Registration successful! You are now logged in.')
             return redirect('home')  
         else:
             messages.error(request, 'Please correct the errors below.')
