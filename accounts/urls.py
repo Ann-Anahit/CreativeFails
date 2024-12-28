7c3d1677bf15723d045f491be74171124fa54d98
Anna Afijan <ann.afijan@gmail.com>
2024-12-28 17:11:22 +0000

    change the urls code for accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from . import views
-from django.contrib.auth import views as auth_views
-from .views import register_view, custom_login, logout_view
+from . import views 
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
--- map/urls.py
+++ map/urls.py
@@ -4,7 +4,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('', views.home_view, name='homepage'),
     path('about/', views.about_view, name='about'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'), 
