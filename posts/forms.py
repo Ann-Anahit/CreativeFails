f3d4971ccbfa603a3dcff3fd12be397c85d76c40
Anna Afijan <ann.afijan@gmail.com>
2024-12-29 11:17:35 +0000

    add two fields to post model

--- posts/forms.py
+++ posts/forms.py
@@ -4,7 +4,7 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'image'] 
+        fields = ['title', 'content', 'image', 'learned_lessons', 'category']
 
 class CommentForm(forms.ModelForm):
     class Meta:
--- posts/models.py
+++ posts/models.py
@@ -12,8 +12,10 @@ class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='posts/', null=True, blank=True)
     likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='post_likes', blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)  
-    updated_at = models.DateTimeField(auto_now=True)  
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    learned_lessons = models.CharField(max_length=255, null=True, blank=True)
+    category = models.CharField(max_length=32, null=True, blank=True)
 
     def __str__(self):
         return self.title
